32e0b0bdea5d65be5c1178b86fabf610710b2908
Eds <eds@edss-mbp-2.home>
2021-02-04 19:01:08 +0000

    Added score history feature

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,16 +1,32 @@
 class Dice
+  attr_reader :record
+
+  def initialize
+  @record = []
+  end
 
   def roll(no_of_rolls = 1)
-    a = [1, 2, 3, 4]
+    a = [1, 2, 3, 4, 5, 6]
     score = 0
     
     no_of_rolls.times do
       score += a.sample
       #p score
     end
-    
+    record(score)
     score
   end
 
+  def record(score)
+    @record.push(score)
+  end
+
+  def view_current_score
+    puts "Current score: #{@record.sum}"
+  end
+
+  def view_history
+    @record.each {|score| puts "#{score}"}
+  end  
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,20 +1,27 @@
 require "dice"
 describe Dice do
-    # let(:subject) { Dice.new(some_argument) } # like a sub test section
-  
-  it "allows the user to roll the dice" do
-    expect(subject).to respond_to(:roll) # checks method is present, not the behavior
+  describe '#roll' do  
+    it "allows the user to roll the dice" do
+      expect(subject).to respond_to(:roll) # checks method is present, not the behavior
+    end
+
+    it "rolls the dice and gives us a number between 1 and 6" do
+      expect(subject.roll).to be_between(1, 6)
+    end
+
+    it "can roll dice multiple times" do
+      # subject.dice_rolls
+      expect(subject.roll(3)).to be_between(3, 18)
+      expect(subject.roll(6)).to be_between(6, 36)
+    end
   end
 
-  it "rolls the dice and gives us a number between 1 and 6" do
-    expect(subject.roll).to be_between(1, 6)
-  end
-
-
-  it "can roll dice multiple times" do
-    # subject.dice_rolls
-    expect(subject.roll(3)).to be_between(3, 18)
-    expect(subject.roll(6)).to be_between(6, 36)
+  describe '#record' do
+    it "records each dice roll" do
+      dice = Dice.new
+      check = dice.roll(1)
+      expect(dice.view_history[0]).to eq check
+    end
   end
 
 end
