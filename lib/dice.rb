39dad25ad07fb13c0ace7b317ae487e2027e9bef
Eds <steadyede@live.co.uk>
2021-02-05 10:24:52 +0000

    Completed game, and refactored. Lovely stuff

--- lib/dice.rb
+++ lib/dice.rb
@@ -2,19 +2,18 @@ class Dice
   attr_reader :record
 
   def initialize
-  @record = []
+    @record = []
+    @dice_six_sides = [1, 2, 3, 4, 5, 6]
+    @score = 0
   end
 
   def roll(no_of_rolls = 1)
-    a = [1, 2, 3, 4, 5, 6]
-    score = 0
-    
     no_of_rolls.times do
-      score += a.sample
+      @score += @dice_six_sides.sample
       #p score
     end
-    record(score)
-    score
+    record(@score)
+    @score
   end
 
   def record(score)
